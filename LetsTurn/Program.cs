cdaf095b8937566ae0865c46b7d8631adba9369e
Dozerhank <65521931+Dozerhank@users.noreply.github.com>
2021-11-01 09:35:58 -0500

    Add files via upload

---
+++ LetsTurn/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/****Why was this so easy****/
+
+public interface ITurnable
+{
+    void Turn();
+}
+
+class Page : ITurnable
+{
+    public Page()
+    {
+
+    }
+
+    public void Turn()
+    {
+        Console.WriteLine("You turn a page in a book");
+    }
+}
+
+class Corner : ITurnable
+{
+    public void Turn()
+    {
+        Console.WriteLine("You turn corners to go around the block");
+    }
+}
+
+class Pancake : ITurnable
+{
+    public void Turn()
+    {
+        Console.WriteLine("You turn a pancake when it's done on one side");
+    }
+}
+
+class Leaf : ITurnable
+{
+    public void Turn()
+    {
+        Console.WriteLine("A leaf turns colors in the fall");
+    }
+}
